4cbe3e6528fb627d75b16dbf55930faa064ed6c0
Tunelko <tunelko@tunelko.com>
2015-11-16 16:35:05 +0100

    suma y media aritmetica
    Ejercicio 2.- Escribir un programa que lea del teclado número a número.
    La lectura de números termina
    con un número negativo. El programa debe mostrar la media aritmética de
    los números introducidos.

--- BUCLES/ejercicio2_bucles_while_media_aritmetica_termina_negativo.py
+++ BUCLES/ejercicio2_bucles_while_media_aritmetica_termina_negativo.py
@@ -5,7 +5,6 @@
 Ejercicio 2.- Escribir un programa que lea del teclado número a número. La lectura de números termina
 con un número negativo. El programa debe mostrar la media aritmética de los números introducidos.
 '''
-
 numeros = 0
 media = 0
 contador = 0
@@ -13,7 +12,7 @@ suma = 0
 
 while numeros >= 0:
     numeros = int(raw_input('Dame el numero:'))
-    if numeros >= 0:
+    if numeros >=   0:
         contador = contador + 1
         suma = suma + numeros
 
