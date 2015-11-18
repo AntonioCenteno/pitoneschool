bd7cf46d31df96b029aebdcc0897f3a8feb3fde5
Tunelko <tunelko@tunelko.com>
2015-11-18 18:46:21 +0100

    restas sucesivas

---
+++ BUCLES/ejercicio6_bucles_while_restas_sucesivas.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Ejercicio 6.- Dados dos enteros a≥0 y b>0, escribe un programa que calcule el cociente y el resto de su
+división entera utilizando para ello el método de restas sucesivas y mostrar el resultado en la forma:
+Cociente: 17 / 3 = 5
+Resto: 17 % 3 = 2
+
+'''
+a = int(raw_input('Dame el numero A:'))
+b = int(raw_input('Dame el numero B:'))
+
+resto=0
+cont=0
+a1=a
+b1=b
+
+while a>=b:
+    a=a-b
+    cont=cont+1
+
+resto=a
+print "Cociente ", a1, "/", b1,"=", cont
+print "Resto ", a1, "%", b1,"=", resto
+
+
+
+
+
