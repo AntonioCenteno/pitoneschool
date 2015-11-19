0f4c64a44d36794c58a2b0132b0f801b8cfc84cd
Tunelko <tunelko@tunelko.com>
2015-11-19 23:35:05 +0100

    fix

--- BUCLES/ejercicio8_bucles_while_cifras_mas_menos_significativas.py
+++ BUCLES/ejercicio8_bucles_while_cifras_mas_menos_significativas.py
@@ -9,11 +9,15 @@ Las cifras del número 7249 de la menos a la más significativa son: 9 4 2 7
 '''
 
 numero = int(raw_input('Numero (Con 0 terminas): '))
+ultima=0
+division=0
+salida=''
 
 while numero > 0:
-    print int(str(numero)[::-1])
-    break;
+    ultima=numero%10
+    numero=numero/10
+    salida+=str(ultima)
 
-
+print salida ," ",
 
 
