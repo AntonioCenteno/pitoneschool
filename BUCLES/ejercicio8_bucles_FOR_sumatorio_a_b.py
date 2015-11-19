6f914ec3b01170c95859eebe4af07c5bda3dc034
Tunelko <tunelko@tunelko.com>
2015-11-19 21:07:01 +0100

    fix

--- BUCLES/ejercicio8_bucles_FOR_sumatorio_a_b.py
+++ BUCLES/ejercicio8_bucles_FOR_sumatorio_a_b.py
@@ -4,13 +4,10 @@
 '''
 Ejercicio 8.- Dados dos enteros a>0 y b, modificar el programa anterior para calcular la expresión
 matemática
-
-sumatorio de i=1..b (i**i+1)/i
+        sumatorio de i=a..b (i**i+1)/i
 
 '''
-a=int(raw_input("a: "))
-b=int(raw_input("b: "))
 
 for i in range(a,b+1):
-    print int((pow(i,2)+1)/i)
+    print float((pow(i,2)+1)/i)
 
