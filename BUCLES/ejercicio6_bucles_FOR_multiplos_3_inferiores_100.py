c5b19ea062bc15193810ebfaf78393e0f8fe4481
Tunelko <tunelko@tunelko.com>
2015-11-19 20:55:27 +0100

    multiplos 3 inferiores 100

---
+++ BUCLES/ejercicio6_bucles_FOR_multiplos_3_inferiores_100.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Ejercicio 6.- Escribir un programa que imprima todos los múltiplos de 3 inferiores a 100.
+
+'''
+
+for i in range(1,100):
+    if i%3==0:
+        print i
+
+
